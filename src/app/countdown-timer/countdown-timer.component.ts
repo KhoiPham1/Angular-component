a4f5c8c26297e248c08fc6a2386cded2dd288976
Khoi Pham <khoi171195@gmail.com>
2019-02-11 09:17:29 +0700

    firstcommit

---
+++ src/app/countdown-timer/countdown-timer.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-countdown-timer',
+  templateUrl: './countdown-timer.component.html',
+  styleUrls: ['./countdown-timer.component.scss']
+})
+export class CountdownTimerComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
